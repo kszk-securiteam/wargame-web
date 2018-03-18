1a0cea12c7c8e985d8a17e9a2b8d84084523d011
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2018-03-19 00:50:45 +0100

    Setup Django templates

--- wargame/urls.py
+++ wargame/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
 
--- wargame/views.py
+++ wargame/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
 
-def index(req):
-    return HttpResponse("Hello world!")
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
