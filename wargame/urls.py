4ea58f15ebb3647853c71b1c0b243a09574280e2
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2018-03-20 18:56:48 +0100

    Added bootstrap, basic navigation, static pages

--- wargame/urls.py
+++ wargame/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('challenges', views.ChallengesView.as_view(), name='challenges'),
+    path('scoreboard', views.ScoreboardView.as_view(), name='scoreboard'),
+    path('rules', views.RulesView.as_view(), name='rules'),
+    path('about_us', views.AboutUsView.as_view(), name='about-us'),
+    path('links', views.LinksView.as_view(), name='links'),
 ]
 
--- wargame/views.py
+++ wargame/views.py
@@ -4,3 +4,22 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
+class ChallengesView(TemplateView):
+    template_name = 'challenges.html'
+
+
+class ScoreboardView(TemplateView):
+    template_name = 'scoreboard.html'
+
+
+class RulesView(TemplateView):
+    template_name = 'rules.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'about_us.html'
+
+
+class LinksView(TemplateView):
+    template_name = 'links.html'
