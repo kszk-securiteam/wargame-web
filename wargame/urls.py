c65f0979cf3dc242dd206c0df83793557d9f2447
Szász Márton <szaszm01@gmail.com>
2018-03-21 21:53:27 +0100

    add login/logout views and some more bootstrap

--- wargame/urls.py
+++ wargame/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import reverse_lazy, path
 
 from . import views
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('rules', views.RulesView.as_view(), name='rules'),
     path('about_us', views.AboutUsView.as_view(), name='about-us'),
     path('links', views.LinksView.as_view(), name='links'),
+    path('logout', LogoutView.as_view(next_page=reverse_lazy('index')), name='logout'),
+    path('login', LoginView.as_view(template_name='login_form.html'), name='login'),
 ]
 
--- wargame_web/settings.py
+++ wargame_web/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.urls import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wargame',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,5 @@ STATIC_URL = '/static/'
 
 # Substitute user model with our own
 AUTH_USER_MODEL = 'wargame.User'
+LOGIN_URL = reverse_lazy('login')
+LOGIN_REDIRECT_URL = reverse_lazy('index')
