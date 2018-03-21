629554216292ee3db55e85ecaa9f9cc156fab034
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2018-03-21 21:20:03 +0100

    Fixed linter problems

--- wargame/models.py
+++ wargame/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class User(AbstractUser):
     pass
 
+
 class File(models.Model):
     path = models.CharField(max_length=512)
     private = models.BooleanField(default=False)
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=64, primary_key=True)
+
 
 class Challenge(models.Model):
     title = models.CharField(max_length=256)
@@ -38,5 +42,3 @@ class Submission(models.Model):
     creation_dt = models.DateTimeField(auto_now_add=True)
     value = models.CharField(max_length=256)
     user_challenge = models.ForeignKey(UserChallenge, on_delete=models.CASCADE)
-
-
