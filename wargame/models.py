cd4ddbc9b3072f935729c64b9fe0665b245772a2
Szász Márton <szaszm01@gmail.com>
2018-04-07 16:07:31 +0200

    implement User.get_score()

--- wargame/models.py
+++ wargame/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.db.models import F, Sum
+from django.db.models.functions import Coalesce
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     def get_score(self):
-        return 0
+        # TODO: qpa/hacktivity switch
+        # SELECT SUM(challenge.points)
+        # FROM user_challenge
+        #   JOIN challenge ON(challenge.id = user_challenge.challenge_id)
+        #   JOIN submission ON (submission.user_challenge_id = user_challenge.id)
+        #   , meta_config
+        # WHERE ((meta_config.is_qpa = '1' AND submission.value = challenge.flag_qpa)
+        #       OR (meta_config.is_hacktivity = '1' AND submission.value = challenge.flag_hacktivity))
+        #   AND user_challenge.user_id = {self.id}
+        return UserChallenge.objects.filter(user=self, submission__value=F('challenge__flag_qpa')) \
+            .only('challenge__points') \
+            .aggregate(total_points=Coalesce(Sum('challenge__points'), 0))['total_points']
 
 
 class File(models.Model):
