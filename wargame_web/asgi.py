30df1097b20bc9ebd095783b6e8c2a6b84fd420a
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2019-06-25 18:36:54 +0200

    ASGI config

---
+++ wargame_web/asgi.py
@@ -0,0 +1,8 @@
+import os
+
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wargame_web.settings.production")
+django.setup()
+application = get_default_application()
